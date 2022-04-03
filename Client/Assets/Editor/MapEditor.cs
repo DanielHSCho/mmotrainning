727f65d9c70b409be51b69e031d12fd9dd32628c
daniel <hellena171791@gmail.com>
2022-04-03 15:19:55 +0900

    <맵 툴> 에디터 기초 작업

---
+++ Client/Assets/Editor/MapEditor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MapEditor
+{
+    #if UNITY_EDITOR
+
+    [MenuItem("Tools/GenerateMap")]
+    private static void HellowWorld()
+    {
+
+    }
+
+    #endif
+}

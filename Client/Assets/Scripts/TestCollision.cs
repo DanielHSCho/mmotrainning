8b3794cf4c2404d073ac435644655ec57a034d09
daniel <hellena171791@gmail.com>
2022-04-03 15:12:19 +0900

    <맵 콜리전> 충돌 검사

--- Client/Assets/Scripts/TestCollision.cs
+++ Client/Assets/Scripts/TestCollision.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TestCollision : MonoBehaviour
 {
+    public Tilemap _tileMap;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class TestCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        List<Vector3Int> blocked = new List<Vector3Int>();
+
+        foreach (Vector3Int pos in _tileMap.cellBounds.allPositionsWithin) {
+            TileBase tile = _tileMap.GetTile(pos);
+            if(tile != null) {
+                blocked.Add(pos);
+            }
+        }
     }
 }

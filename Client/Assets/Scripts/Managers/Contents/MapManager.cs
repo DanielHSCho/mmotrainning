5fe7dd2f0dbdcf65957eb8dae1a189ae3378f2ed
daniel <hellena171791@gmail.com>
2022-04-04 15:55:32 +0900

    <맵 매니저 추가>

--- Client/Assets/Scripts/Managers/Contents/MapManager.cs
+++ Client/Assets/Scripts/Managers/Contents/MapManager.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class MapManager
 {
-    
+    public Grid CurrentGrid { get; private set; }
+
+    public void LoadMap(int mapId)
+    {
+        DestroyMap();
+
+        string mapName = "Map_" + mapId.ToString("000");
+        GameObject go = Managers.Resource.Instantiate($"Map/{mapName}");
+        go.name = "Map";
+
+        GameObject collision = Util.FindChild(go, "Tilemap_Collision", true);
+        if(collision != null) {
+            collision.SetActive(false);
+        }
+
+        CurrentGrid = go.GetComponent<Grid>();
+    }
+
+    public void DestroyMap()
+    {
+        GameObject map = GameObject.Find("Map");
+        if(map != null) {
+            GameObject.Destroy(map);
+            CurrentGrid = null;
+        }
+    }
 }

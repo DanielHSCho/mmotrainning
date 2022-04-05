21fc3291c9c8a582e2ffe7541709b30fdf82c84c
daniel <hellena171791@gmail.com>
2022-04-05 15:05:58 +0900

    <정찰 AI> 이동 관련 부분 추가

--- Client/Assets/Scripts/Controllers/MonsterController.cs
+++ Client/Assets/Scripts/Controllers/MonsterController.cs
@@ -6,6 +6,7 @@ using static Define;
 public class MonsterController : CreatureController
 {
     Coroutine _coPatrol;
+    Vector3Int _destCellPos;
 
     protected override void Init()
     {
@@ -20,6 +21,47 @@ public class MonsterController : CreatureController
 
         if(_coPatrol == null) {
             _coPatrol = StartCoroutine("CoPatrol");
+        }
+    }
+
+    protected override void MoveToNextPos()
+    {
+        Vector3Int moveCellDir = _destCellPos - CellPos;
+        // TODO : Astar
+
+        if(moveCellDir.x > 0) {
+            Dir = MoveDir.Right;
+        } else if (moveCellDir.x < 0) {
+            Dir = MoveDir.Left;
+        } else if (moveCellDir.y > 0) {
+            Dir = MoveDir.Up;
+        } else if (moveCellDir.y < 0) {
+            Dir = MoveDir.Down;
+        } else {
+            Dir = MoveDir.None;
+        }
+
+        Vector3Int destPos = CellPos;
+
+        switch (_dir) {
+            case MoveDir.Up:
+                destPos += Vector3Int.up;
+                break;
+            case MoveDir.Down:
+                destPos += Vector3Int.down;
+                break;
+            case MoveDir.Left:
+                destPos += Vector3Int.left;
+                break;
+            case MoveDir.Right:
+                destPos += Vector3Int.right;
+                break;
+        }
+
+        if (Managers.Map.CanGo(destPos) && Managers.Object.Find(destPos) == null) {
+            CellPos = destPos;
+        } else {
+            State = CreatureState.Idle;
         }
     }
 
@@ -48,8 +90,13 @@ public class MonsterController : CreatureController
 
             // 이동이 가능하고, 아무 오브젝트가 없다면 이동
             if(Managers.Map.CanGo(randomPos) && Managers.Object.Find(randomPos) == null){
-                
+                _destCellPos = randomPos;
+                State = CreatureState.Moving;
+                yield break;
             }
         }
+
+        // 시도해봤지만 목적지를 못찾음 -> 대기상태 전환
+        State = CreatureState.Idle;
     }
 }

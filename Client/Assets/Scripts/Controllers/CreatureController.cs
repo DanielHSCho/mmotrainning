71826838cd9fb36d4f3b7fad0d11f540d56a31f8
daniel <hellena171791@gmail.com>
2022-04-05 12:47:55 +0900

    <코드 정리>

--- Client/Assets/Scripts/Controllers/CreatureController.cs
+++ Client/Assets/Scripts/Controllers/CreatureController.cs
@@ -141,29 +141,7 @@ public class CreatureController : MonoBehaviour
         }
     }
 
-    void UpdateMoving()
-    {
-        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
-        Vector3 moveDir = destPos - transform.position;
-
-        // 도착 여부 체크
-        float dist = moveDir.magnitude;
-        // 거의 도착했다면
-        if (dist < _speed * Time.deltaTime) {
-            transform.position = destPos;
-
-            // 예외적으로 애니메이션 컨트롤
-            _state = CreatureState.Idle;
-            if(_dir == MoveDir.None) {
-                UpdateAnimation();
-            }
-        } else {
-            transform.position += moveDir.normalized * _speed * Time.deltaTime;
-            State = CreatureState.Moving;
-        }
-    }
-
-    void UpdateIdle()
+    protected virtual void UpdateIdle()
     {
         if (_dir != MoveDir.None) {
             Vector3Int destPos = CellPos;
@@ -186,10 +164,42 @@ public class CreatureController : MonoBehaviour
             State = CreatureState.Moving;
 
             if (Managers.Map.CanGo(destPos)) {
-                if(Managers.Object.Find(destPos) == null) {
+                if (Managers.Object.Find(destPos) == null) {
                     CellPos = destPos;
                 }
             }
         }
+    }
+
+    protected virtual void UpdateMoving()
+    {
+        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
+        Vector3 moveDir = destPos - transform.position;
+
+        // 도착 여부 체크
+        float dist = moveDir.magnitude;
+        // 거의 도착했다면
+        if (dist < _speed * Time.deltaTime) {
+            transform.position = destPos;
+
+            // 예외적으로 애니메이션 컨트롤
+            _state = CreatureState.Idle;
+            if(_dir == MoveDir.None) {
+                UpdateAnimation();
+            }
+        } else {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            State = CreatureState.Moving;
+        }
+    }
+
+    protected virtual void UpdateSkill()
+    {
+
+    }
+
+    protected virtual void UpdateDead()
+    {
+
     }
 }
--- Client/Assets/Scripts/Controllers/PlayerController.cs
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -25,16 +25,12 @@ public class PlayerController : CreatureController
     {
         // TODO : 이동 부분은 인풋 매니저화 해야함
         if (Input.GetKey(KeyCode.W)) {
-            //transform.position += Vector3.up * Time.deltaTime * _speed;
             Dir = MoveDir.Up;
         } else if (Input.GetKey(KeyCode.S)) {
-            //transform.position += Vector3.down * Time.deltaTime * _speed;
             Dir = MoveDir.Down;
         } else if (Input.GetKey(KeyCode.A)) {
-            //transform.position += Vector3.left * Time.deltaTime * _speed;
             Dir = MoveDir.Left;
         } else if (Input.GetKey(KeyCode.D)) {
-            //transform.position += Vector3.right * Time.deltaTime * _speed;
             Dir = MoveDir.Right;
         } else {
             Dir = MoveDir.None;

d95144aaa35349c66317fd620c3cec406ce89d44
daniel <hellena171791@gmail.com>
2022-04-05 14:57:16 +0900

    <정찰 AI - Idle상태에서 정찰 시작 / 정찰 목적지 판단>

--- Client/Assets/Scripts/Controllers/MonsterController.cs
+++ Client/Assets/Scripts/Controllers/MonsterController.cs
@@ -5,11 +5,22 @@ using static Define;
 
 public class MonsterController : CreatureController
 {
+    Coroutine _coPatrol;
+
     protected override void Init()
     {
         base.Init();
         State = CreatureState.Idle;
         Dir = MoveDir.None;
+    }
+
+    protected override void UpdateIdle()
+    {
+        base.UpdateIdle();
+
+        if(_coPatrol == null) {
+            _coPatrol = StartCoroutine("CoPatrol");
+        }
     }
 
     public override void OnDamaged()
@@ -23,5 +34,22 @@ public class MonsterController : CreatureController
         // TODO : 나중에 오브젝트 매니저에서 한번에 처리할 수 있도록 개선해야함
         Managers.Object.Remove(this.gameObject);
         Managers.Resource.Destroy(this.gameObject);
+    }
+
+    IEnumerator CoPatrol()
+    {
+        int waitSeconds = Random.Range(1, 4);
+        yield return new WaitForSeconds(waitSeconds);
+
+        for(int i = 0; i < 10; i++) {
+            int xRange = Random.Range(-5, 6);
+            int yRange = Random.Range(-5, 6);
+            Vector3Int randomPos = CellPos + new Vector3Int(xRange, yRange, 0);
+
+            // 이동이 가능하고, 아무 오브젝트가 없다면 이동
+            if(Managers.Map.CanGo(randomPos) && Managers.Object.Find(randomPos) == null){
+                
+            }
+        }
     }
 }

a185e0091945e5a2751aa65ded0025bf963bc0a6
daniel <hellena171791@gmail.com>
2022-04-06 14:21:23 +0900

    <프로토 버프> 프로토버프 적용부분 ClientSession의 OnConnected로 이동

--- Server/Server/Session/ClientSession.cs
+++ Server/Server/Session/ClientSession.cs
@@ -6,6 +6,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using ServerCore;
 using System.Net;
+using Google.Protobuf.Protocol;
+using static Google.Protobuf.Protocol.Person.Types;
+using Google.Protobuf;
 
 namespace Server
 {
@@ -16,6 +19,22 @@ namespace Server
 		public override void OnConnected(EndPoint endPoint)
 		{
 			Console.WriteLine($"OnConnected : {endPoint}");
+
+			Person person = new Person() {
+				Name = "Daniel",
+				Id = 123,
+				Email = "daniel@naver.com",
+				Phones = { new PhoneNumber { Number = "555-4321", Type = Person.Types.PhoneType.Home } }
+			};
+
+			ushort size = (ushort)person.CalculateSize();
+			byte[] sendBuffer = new byte[size + 4];
+			Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
+			ushort protocolId = 1;
+			Array.Copy(BitConverter.GetBytes(protocolId), 0, sendBuffer, 2, sizeof(ushort));
+			Array.Copy(person.ToByteArray(), 0, sendBuffer, 4, sizeof(ushort));
+
+			Send(new ArraySegment<byte>(sendBuffer));
 		}
 
 		public override void OnRecvPacket(ArraySegment<byte> buffer)

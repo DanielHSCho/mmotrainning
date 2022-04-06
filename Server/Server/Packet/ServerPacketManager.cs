a89ec18d651914771f9f4ede0ccc0148bed210e1
daniel <hellena171791@gmail.com>
2022-04-06 14:21:51 +0900

    <프로토 버프> 패킷매니저에 IPacket -> IMessage로 변경

--- Server/Server/Packet/ServerPacketManager.cs
+++ Server/Server/Packet/ServerPacketManager.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf;
 using ServerCore;
 using System;
 using System.Collections.Generic;
@@ -15,12 +16,12 @@ class PacketManager
 	}
 
 	Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>> _onRecv = new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>>();
-	Dictionary<ushort, Action<PacketSession, IPacket>> _handler = new Dictionary<ushort, Action<PacketSession, IPacket>>();
+	Dictionary<ushort, Action<PacketSession, IMessage>> _handler = new Dictionary<ushort, Action<PacketSession, IMessage>>();
 		
 	public void Register()
 	{
-		_onRecv.Add((ushort)PacketID.C_Chat, MakePacket<C_Chat>);
-		_handler.Add((ushort)PacketID.C_Chat, PacketHandler.C_ChatHandler);
+		_onRecv.Add((ushort)IMessage.C_Chat, MakePacket<C_Chat>);
+		_handler.Add((ushort)IMessage.C_Chat, PacketHandler.C_ChatHandler);
 
 	}
 
@@ -38,11 +39,11 @@ class PacketManager
 			action.Invoke(session, buffer);
 	}
 
-	void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IPacket, new()
+	void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IMessage, new()
 	{
 		T pkt = new T();
 		pkt.Read(buffer);
-		Action<PacketSession, IPacket> action = null;
+		Action<PacketSession, IMessage> action = null;
 		if (_handler.TryGetValue(pkt.Protocol, out action))
 			action.Invoke(session, pkt);
 	}

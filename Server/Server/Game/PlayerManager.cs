fd730d23b392dacbda2f85c3b20731b56beb2d2a
daniel <hellena171791@gmail.com>
2022-04-07 15:31:09 +0900

    <서버 - 게임 입장> 플레이어 매니저

--- Server/Server/Game/PlayerManager.cs
+++ Server/Server/Game/PlayerManager.cs
@@ -6,5 +6,42 @@ namespace Server.Game
 {
     public class PlayerManager
     {
+        public static PlayerManager Instance { get; } = new PlayerManager();
+
+        object _lock = new object();
+        Dictionary<int, Player> _players = new Dictionary<int, Player>();
+        int _playerId = 1; // TODO - 비트 플래그로 쪼개서 사용하기도 함
+
+        public Player Add()
+        {
+            Player player = new Player();
+
+            lock (_lock) {
+                player.Info.PlayerId = _playerId;
+                _players.Add(_playerId, player);
+                _playerId++;
+            }
+
+            return player;
+        }
+
+        public bool Remove(int playerId)
+        {
+            lock (_lock) {
+                return _players.Remove(playerId);
+            }
+        }
+
+        public Player Find(int playerId)
+        {
+            lock (_lock) {
+                Player player = null;
+                if (_players.TryGetValue(playerId, out player)) {
+                    return player;
+                }
+
+                return null;
+            }
+        }
     }
 }

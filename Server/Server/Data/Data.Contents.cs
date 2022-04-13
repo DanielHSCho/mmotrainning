ba74927a01b55859d1231d6a085aeb91925f76ec
daniel <hellena171791@gmail.com>
2022-04-13 15:56:21 +0900

    <서버 - Data_Config> 스킬 오브젝트형 데이터 모델링 / 스킬 데이터 Load 구현

--- Server/Server/Data/Data.Contents.cs
+++ Server/Server/Data/Data.Contents.cs
@@ -40,6 +40,29 @@ namespace Server.Data
 		public int damage;
 
 		public SkillType skillType;
+		public ProjectileInfo projectile;
     }
-    #endregion
+
+	public class ProjectileInfo
+    {
+		public string name;
+		public float speed;
+		public int range;
+		public string prefab;
+    }
+
+	[Serializable]
+	public class SkillData : ILoader<int, Skill>
+	{
+		public List<Skill> skills = new List<Skill>();
+
+		public Dictionary<int, Skill> MakeDict()
+		{
+			Dictionary<int, Skill> dict = new Dictionary<int, Skill>();
+			foreach (Skill skill in skills)
+				dict.Add(skill.id, skill);
+			return dict;
+		}
+	}
+	#endregion
 }

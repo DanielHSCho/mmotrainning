f185718c25c1d310c8697c5c1a33c0359b86446b
daniel <hellena171791@gmail.com>
2022-04-13 17:23:03 +0900

    <서버 - 스탯> 클라쪽에 서버에서 능력치 받아오도록 수정

--- Client/Assets/Scripts/Managers/Contents/ObjectManager.cs
+++ Client/Assets/Scripts/Managers/Contents/ObjectManager.cs
@@ -29,6 +29,8 @@ public class ObjectManager
                 MyPlayer = go.GetComponent<MyPlayerController>();
                 MyPlayer.Id = info.ObjectId;
                 MyPlayer.PosInfo = info.PosInfo;
+                MyPlayer.Stat = info.StatInfo;
+
                 MyPlayer.SyncPos();
             } else {
                 GameObject go = Managers.Resource.Instantiate("Creature/Player");
@@ -38,6 +40,7 @@ public class ObjectManager
                 PlayerController pc = go.GetComponent<PlayerController>();
                 pc.Id = info.ObjectId;
                 pc.PosInfo = info.PosInfo;
+                pc.Stat = info.StatInfo;
                 pc.SyncPos();
             }
         } else if(objectType == GameObjectType.Monster) {
@@ -48,8 +51,8 @@ public class ObjectManager
             _objects.Add(info.ObjectId, go);
 
             ArrowController ac = go.GetComponent<ArrowController>();
-            ac.Dir = info.PosInfo.MoveDir;
-            ac.CellPos = new Vector3Int(info.PosInfo.PosX, info.PosInfo.PosY, 0);
+            ac.PosInfo = info.PosInfo;
+            ac.Stat = info.StatInfo;
             ac.SyncPos();
         }
     }

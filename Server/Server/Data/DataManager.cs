0cd5ae84aecbef70181156c49d048141195e6e7f
daniel <hellena171791@gmail.com>
2022-04-13 15:26:44 +0900

    <서버 - Data_Config> 데이터 매니저 Config 정보 받기 / File 함수 파일 형식까지 명시

--- Server/Server/Data/DataManager.cs
+++ Server/Server/Data/DataManager.cs
@@ -21,7 +21,7 @@ namespace Server.Data
 
         Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
         {
-            string text = File.ReadAllText($"");
+            string text = File.ReadAllText($"{ConfigManager.Config.dataPath}/{path}.json");
             return Newtonsoft.Json.JsonConvert.DeserializeObject<Loader>(text);
         }
     }

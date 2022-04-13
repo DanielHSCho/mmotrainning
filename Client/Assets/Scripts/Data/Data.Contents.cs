8d75b2d5e66c38906d6ed051346dcf7d1c0e65a4
daniel <hellena171791@gmail.com>
2022-04-13 16:06:39 +0900

    <클라 - Data_Config> 클라에도 스킬 데이터 모델링

--- Client/Assets/Scripts/Data/Data.Contents.cs
+++ Client/Assets/Scripts/Data/Data.Contents.cs
@@ -29,4 +29,40 @@ namespace Data
 		}
 	}
 	#endregion
+
+	#region Skill
+	[Serializable]
+	public class Skill
+	{
+		public int id;
+		public string name;
+		public float cooldown;
+		public int damage;
+
+		public SkillType skillType;
+		public ProjectileInfo projectile;
+	}
+
+	public class ProjectileInfo
+	{
+		public string name;
+		public float speed;
+		public int range;
+		public string prefab;
+	}
+
+	[Serializable]
+	public class SkillData : ILoader<int, Skill>
+	{
+		public List<Skill> skills = new List<Skill>();
+
+		public Dictionary<int, Skill> MakeDict()
+		{
+			Dictionary<int, Skill> dict = new Dictionary<int, Skill>();
+			foreach (Skill skill in skills)
+				dict.Add(skill.id, skill);
+			return dict;
+		}
+	}
+	#endregion
 }

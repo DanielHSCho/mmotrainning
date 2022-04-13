782fae004e72f5e387892051a47ac4fb6d8427a4
daniel <hellena171791@gmail.com>
2022-04-13 15:22:26 +0900

    <서버 - Data_Congig> Config 매니저 구현 / 실행파일 경로에 config.json 파일 추가 (+필요)
    - config.json 파일은
    -
    {
      "dataPath": "../../../../../Client/Assets/Resources/Data"
    }

--- Server/Server/Data/ConfigManager.cs
+++ Server/Server/Data/ConfigManager.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Server.Data
 {
+    [Serializable]
+    public class ServerConfig
+    {
+        public string dataPath;
+
+        // TODO : 최대 동접 / 포트 번호도 이곳에서 관리
+    }
+
     class ConfigManager
     {
+        public static ServerConfig Config { get; private set; }
+        public static void LoadConfig()
+        {
+            string text = File.ReadAllText("config.json");
+            Config = Newtonsoft.Json.JsonConvert.DeserializeObject<ServerConfig>(text);
+        }
     }
 }

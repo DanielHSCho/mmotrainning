1c9e338c971f2ba303ef0ef6fedb9db2e3f44a7a
daniel <hellena171791@gmail.com>
2022-04-17 15:43:42 +0900

    <서버 - SearchAI> 몬스터 FSM(유한 상태 기계) 방식 AI 적용

--- Server/Server/Game/Object/Monster.cs
+++ Server/Server/Game/Object/Monster.cs
@@ -7,9 +7,61 @@ namespace Server.Game
 {
     public class Monster : GameObject
     {
+        long _nextSearchTick = 0;
+
         public Monster()
         {
             ObjectType = GameObjectType.Monster;
+
+            // TEMP
+            Stat.Level = 1;
+            Stat.Hp = 100;
+            Stat.MaxHp = 100;
+            Stat.Speed = 5.0f;
+
+            State = CreatureState.Idle;
+        }
+
+        // FSM 방식
+        public override void Update()
+        {
+            switch (State) {
+                case CreatureState.Idle:
+                    UpdateIdle();
+                    break;
+                case CreatureState.Moving:
+                    UpdateMoving();
+                    break;
+                case CreatureState.Skill:
+                    UpdateSkill();
+                    break;
+                case CreatureState.Dead:
+                    UpdateDead();
+                    break;
+            }
+        }
+
+        protected virtual void UpdateIdle()
+        {
+            if(_nextSearchTick > Environment.TickCount64) {
+                return;
+            }
+
+            _nextSearchTick = Environment.TickCount64 + 1000;
+        }
+
+        protected virtual void UpdateMoving()
+        {
+
+        }
+        protected virtual void UpdateSkill()
+        {
+
+        }
+
+        protected virtual void UpdateDead()
+        {
+
         }
     }
 }

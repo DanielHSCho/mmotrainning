f459077ddcf96e794b134b654d2a9b584d5cab76
daniel <hellena171791@gmail.com>
2022-04-18 14:36:13 +0900

    <서버 - 커맨드패턴> IJob 구현 (+주석)
    - 기존에 패킷핸들러가 직접 함수 실행해서 처리하는 부분들을 커맨드패턴을 사용해 주문내역으로 캡슐화 하자
    - 그 실행함수들을 캡슐화하기 위해 IJob 인터페이스 구현

---
+++ Server/Server/Game/Job/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Game
+{
+    public interface IJob
+    {
+        void Execute();
+    }
+
+    public class Job : IJob
+    {
+
+    }
+}

6e4271f41674ac7bbf116d5bff9a3284739b8223
daniel <hellena171791@gmail.com>
2022-04-25 13:36:24 +0900

    <서버 - 접속> 클라 로그인 요청시 패킷핸들링 함수 구현 (C_LoginHandler)

--- Server/Server/Packet/PacketHandler.cs
+++ Server/Server/Packet/PacketHandler.cs
@@ -1,10 +1,12 @@
 ﻿using Google.Protobuf;
 using Google.Protobuf.Protocol;
 using Server;
+using Server.DB;
 using Server.Game;
 using ServerCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 class PacketHandler
@@ -43,5 +45,35 @@ class PacketHandler
 		}
 
 		room.Push(room.HandleSkill, player, skillPacket);
+	}
+
+	public static void C_LoginHandler(PacketSession session, IMessage packet)
+	{
+		C_Login loginPacket = packet as C_Login;
+		ClientSession clientSession = session as ClientSession;
+
+		// TODO : 콘솔도 나중엔 체계화 해야함
+		Console.WriteLine($"UniqueId({loginPacket.UniqueId})");
+
+		// TODO : 이런저런 보안 체크
+
+		// Account 존재 여부 체크
+		// TODO : 단, 이 로직은 문제가 있음
+		using (AppDbContext db = new AppDbContext()) {
+			AccountDb findAccount = db.Accounts
+				.Where(a => a.AccountName == loginPacket.UniqueId).FirstOrDefault();
+
+			if (findAccount != null) {
+				S_Login loginOk = new S_Login() { LoginOk = 1 };
+				clientSession.Send(loginOk);
+			} else {
+				AccountDb newAccount = new AccountDb() { AccountName = loginPacket.UniqueId };
+				db.Accounts.Add(newAccount);
+				db.SaveChanges();
+
+				S_Login loginOk = new S_Login() { LoginOk = 1 };
+				clientSession.Send(loginOk);
+			}
+		}
 	}
 }

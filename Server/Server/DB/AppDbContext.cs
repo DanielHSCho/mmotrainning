c2eebfee1c74a932f2e706b1bb705047b9f9f42f
daniel <hellena171791@gmail.com>
2022-04-25 12:40:43 +0900

    <DB 연동> 연결 문자열 받아옴

--- Server/Server/DB/AppDbContext.cs
+++ Server/Server/DB/AppDbContext.cs
@@ -13,11 +13,13 @@ namespace Server.DB
 
         static readonly ILoggerFactory _logger = LoggerFactory.Create(builder => { builder.AddConsole(); });
 
+        string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options
                 .UseLoggerFactory(_logger)
-                .UseSqlServer("");
+                .UseSqlServer(_connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder builder)

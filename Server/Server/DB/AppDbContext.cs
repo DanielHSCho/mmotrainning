9522a19240f238ed8befd9e99bba756f8c1b07bb
daniel <hellena171791@gmail.com>
2022-04-25 12:32:13 +0900

    <DB 연동> AppDbContext에 DbSet / OnConfiguring 함수 구현

--- Server/Server/DB/AppDbContext.cs
+++ Server/Server/DB/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,17 @@ namespace Server.DB
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<AccountDb> Accounts { get; set; }
+        public DbSet<PlayerDb> Players { get; set; }
+
+        static readonly ILoggerFactory _logger = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options
+                .UseLoggerFactory(_logger)
+                .UseSqlServer("");
+        }
+
     }
 }

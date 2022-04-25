c56cb442dc92ea8598084151daf21d9530920406
daniel <hellena171791@gmail.com>
2022-04-25 12:12:09 +0900

    <DB 연동> 폴더 / 파일 추가

---
+++ Server/Server/DB/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.DB
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}

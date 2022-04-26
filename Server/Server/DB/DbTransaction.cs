297c377d2c4bd75b73a10f1e0dceba4454b6d3f8
daniel <hellena171791@gmail.com>
2022-04-26 12:24:27 +0900

    <서버 - Hp연동 #2> 람다로 Db일감 전달해서 콜백 받는 로직 추가 (+)
    - GameRoom -> DbTransaction -> GameRoom(콜백) 순으로 동작

--- Server/Server/DB/DbTransaction.cs
+++ Server/Server/DB/DbTransaction.cs
@@ -1,4 +1,5 @@
-﻿using Server.Game;
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Game;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,32 @@ namespace Server.DB
 {
     public class DbTransaction : JobSerializer
     {
+        public static DbTransaction Instance { get; } = new DbTransaction();
 
+        // Me(GameRoom) -> You(Db) -> Me(GameRoom)
+        public static void SavePlayerStatus_AllInOne(Player player, GameRoom room)
+        {
+            if(player == null || room == null) {
+                return;
+            }
+
+            // Me (GameRoom)
+            PlayerDb playerDb = new PlayerDb();
+            playerDb.PlayerDbId = player.PlayerDbId;
+            playerDb.Hp = player.Stat.Hp;
+
+            // You (DB직원에 요청)
+            Instance.Push(() => {
+                using (AppDbContext db = new AppDbContext()) {
+                    db.Entry(playerDb).State = EntityState.Unchanged;
+                    db.Entry(playerDb).Property(nameof(playerDb.Hp)).IsModified = true;
+                    bool success = db.SaveChangesEx();
+                    if (success) {
+                        // Me (GameRoom) - callback
+                        room.Push(() => Console.WriteLine($"Hp Saved({playerDb.Hp})"));
+                    }
+                }
+            });
+        }
     }
 }

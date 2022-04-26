fe9ceba0524838a267419bb3dc26e750cd2d3cfb
daniel <hellena171791@gmail.com>
2022-04-26 17:49:47 +0900

    <서버 - Inven #2> 아이템 슬롯 정보도 Db에서 불러오도록 추가

--- Server/Server/Game/Item/Item.cs
+++ Server/Server/Game/Item/Item.cs
@@ -29,6 +29,12 @@ namespace Server.Game
             set { Info.Count = value; }
         }
 
+        public int Slot
+        {
+            get { return Info.Slot; }
+            set { Info.Slot = value; }
+        }
+
         public ItemType ItemType { get; private set; }
         public bool Stackable { get; protected set; }
 
@@ -63,6 +69,7 @@ namespace Server.Game
             if(item != null) {
                 item.ItemDbId = itemDb.ItemDbId;
                 item.Count = itemDb.Count;
+                item.Slot = itemDb.Slot;
             }
 
             return item;

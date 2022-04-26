26069a379cca79938059336c65186adf633e3c5c
daniel <hellena171791@gmail.com>
2022-04-26 14:02:55 +0900

    <서버 - Item #1> 아이템 데이터 모델링 / 데이터매니저에 아이템 Dictionary 로드 추가

--- Server/Server/Data/Data.Contents.cs
+++ Server/Server/Data/Data.Contents.cs
@@ -58,4 +58,64 @@ namespace Server.Data
 		}
 	}
 	#endregion
+
+	#region Item
+	[Serializable]
+	public class ItemData
+	{
+		public int id;
+		// TODO : 글로벌 서비스 운영시 해당 부분을 string이 아니라
+		// int 같은 것으로 국가 코드를 지정해서 해당 문자열을 받아와야 함
+		public string name;
+		public ItemType itemType;
+	}
+
+	public class WeaponData : ItemData
+    {
+		public WeaponType weaponType;
+		public int damage;
+    }
+
+	public class ArmorData : ItemData
+    {
+		public ArmorType armorType;
+		public int defence;
+    }
+
+	public class ConsumableData : ItemData
+    {
+		public ConsumableType consumableType;
+		public int maxCount;
+    }
+
+
+	[Serializable]
+	public class ItemLoader : ILoader<int, ItemData>
+	{
+		public List<WeaponData> weapons = new List<WeaponData>();
+		public List<ArmorData> armors = new List<ArmorData>();
+		public List<ConsumableData> consumables = new List<ConsumableData>();
+
+		public Dictionary<int, ItemData> MakeDict()
+		{
+			Dictionary<int, ItemData> dict = new Dictionary<int, ItemData>();
+			foreach (ItemData item in weapons) {
+				item.itemType = ItemType.Weapon;
+				dict.Add(item.id, item);
+			}
+
+			foreach (ItemData item in armors) {
+				item.itemType = ItemType.Armor;
+				dict.Add(item.id, item);
+			}
+
+			foreach (ItemData item in consumables) {
+				item.itemType = ItemType.Consumable;
+				dict.Add(item.id, item);
+			}
+
+			return dict;
+		}
+	}
+	#endregion
 }

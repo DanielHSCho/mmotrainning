06b6b69886e8e1eecbe364cab63d7f99b69b9ab6
daniel <hellena171791@gmail.com>
2022-04-26 12:04:14 +0900

    <서버 - Hp연동 #2> Db도 일감형태로 처리하도록 DbTransaction 추가

---
+++ Server/Server/DB/DbTransaction.cs
@@ -0,0 +1,12 @@
+﻿using Server.Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.DB
+{
+    public class DbTransaction : JobSerializer
+    {
+
+    }
+}

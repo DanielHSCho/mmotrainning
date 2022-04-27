0d3b90e7eee33427039445a3becc4dffe1dbd884
daniel <hellena171791@gmail.com>
2022-04-27 17:32:39 +0900

    <클라 - 아이템장착 #2> 아이템 UI에서 item 정보를 들고있도록 개선

--- Client/Assets/Scripts/UI/Scene/UI_Inventory_Item.cs
+++ Client/Assets/Scripts/UI/Scene/UI_Inventory_Item.cs
@@ -12,23 +12,34 @@ public class UI_Inventory_Item : UI_Base
     [SerializeField]
     Image _frame = null;
 
+    public int ItemDbId { get; private set; }
+    public int TemplateId { get; private set; }
+    public int Count { get; private set; }
+    public bool Equipped { get; private set; }
+
+
     public override void Init()
     {
         _icon.gameObject.BindEvent((e) => {
             Debug.Log("Click Item");
 
             C_EquipItem equipPacket = new C_EquipItem();
-            equipPacket.ItemDbId = 0;
-            equipPacket.Equipped = true;
+            equipPacket.ItemDbId = ItemDbId;
+            equipPacket.Equipped = !Equipped; // 토글방식
 
             Managers.Network.Send(equipPacket);
         });
     }
 
-    public void SetItem(int templateId, int count)
+    public void SetItem(Item item)
     {
+        ItemDbId = item.ItemDbId;
+        TemplateId = item.TemplateId;
+        Count = item.Count;
+        Equipped = item.Equipped;
+
         Data.ItemData itemData = null;
-        Managers.Data.ItemDict.TryGetValue(templateId, out itemData);
+        Managers.Data.ItemDict.TryGetValue(TemplateId, out itemData);
 
         Sprite icon = Managers.Resource.Load<Sprite>(itemData.iconPath);
         _icon.sprite = icon;

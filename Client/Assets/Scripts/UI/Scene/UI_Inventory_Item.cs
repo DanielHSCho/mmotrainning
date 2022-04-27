fea64549c3f36bff56645562477f3966e9f17fa0
daniel <hellena171791@gmail.com>
2022-04-27 17:24:32 +0900

    <클라 - 아이템장착 #2> 아이템 아이콘 클릭 시 서버에 장착 요청

--- Client/Assets/Scripts/UI/Scene/UI_Inventory_Item.cs
+++ Client/Assets/Scripts/UI/Scene/UI_Inventory_Item.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,20 @@ public class UI_Inventory_Item : UI_Base
     [SerializeField]
     Image _icon = null;
 
+    [SerializeField]
+    Image _frame = null;
+
     public override void Init()
     {
+        _icon.gameObject.BindEvent((e) => {
+            Debug.Log("Click Item");
+
+            C_EquipItem equipPacket = new C_EquipItem();
+            equipPacket.ItemDbId = 0;
+            equipPacket.Equipped = true;
+
+            Managers.Network.Send(equipPacket);
+        });
     }
 
     public void SetItem(int templateId, int count)

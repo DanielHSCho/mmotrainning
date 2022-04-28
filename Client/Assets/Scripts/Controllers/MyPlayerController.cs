3df26b73bb12c518a92bb38939b68c7c658d6c92
daniel <hellena171791@gmail.com>
2022-04-28 14:53:47 +0900

    <클라 - 스탯UI> 스탯창 여닫기

--- Client/Assets/Scripts/Controllers/MyPlayerController.cs
+++ Client/Assets/Scripts/Controllers/MyPlayerController.cs
@@ -78,6 +78,19 @@ public class MyPlayerController : PlayerController
                 invenUI.gameObject.SetActive(true);
                 invenUI.RefreshUI();
             }
+        } else if (Input.GetKeyDown(KeyCode.C)) {
+            UI_GameScene gameSceneUI = Managers.UI.SceneUI as UI_GameScene;
+            if (gameSceneUI == null) {
+                return;
+            }
+            UI_Stat statUI = gameSceneUI.StatUI;
+
+            if (statUI.gameObject.activeSelf) {
+                statUI.gameObject.SetActive(false);
+            } else {
+                statUI.gameObject.SetActive(true);
+                statUI.RefreshUI();
+            }
         }
     }
 

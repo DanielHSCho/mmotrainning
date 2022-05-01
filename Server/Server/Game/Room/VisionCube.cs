7cb58a9e33b9ac97b80d290a817a73151691dd18
daniel <hellena171791@gmail.com>
2022-05-01 14:38:54 +0900

    <서버 - Zone #2> 비전큐브 구현 / Player에 적용

---
+++ Server/Server/Game/Room/VisionCube.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Game
+{
+    public class VisionCube
+    {
+        // Note : 1초에 한번씩 체크하면서 실시간 스폰/디스폰 관리
+        public Player Owner { get; private set; }
+        public HashSet<GameObject> PreviousObjects { get; private set; }
+
+        public VisionCube(Player owner)
+        {
+            Owner = owner;
+        }
+    }
+}

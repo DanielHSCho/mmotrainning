6b8edd0e57774952d7873c0df82693486376fdd4
daniel <hellena171791@gmail.com>
2022-05-05 12:06:56 +0900

    <서버 - 계정서버 #1> DB 데이터 모델링

--- Server/AccountServer/DB/DataModel.cs
+++ Server/AccountServer/DB/DataModel.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AccountServer.DB
 {
-   
+    [Table("Account")]
+    public class AccountDb
+    {
+        public int AccountDbId { get; set; }
+        public string AccountName { get; set; }
+
+        // TODO : 이메일 주소 / 이메일 인증 여부 / 비밀번호는 해쉬로
+        public string Password { get; set; }
+    }
+
 }

3c9cbb8a34a7da3587a99eca3c69df6f6cef9168
daniel <hellena171791@gmail.com>
2022-05-05 14:33:47 +0900

    <클라 - 로그인 UI> 계정생성 / 로그인 버튼 연동

--- Client/Assets/Scripts/UI/Scene/UI_LoginScene.cs
+++ Client/Assets/Scripts/UI/Scene/UI_LoginScene.cs
@@ -31,11 +31,35 @@ public class UI_LoginScene : UI_Scene
 
     public void OnClickCreateButton(PointerEventData evt)
     {
-        Debug.Log("OnClickCreateButton");
+        string account = GetText((int)Texts.AccountNameText).text;
+        string password = GetText((int)Texts.PasswordText).text;
+
+        // TODO : WebPacket쪽 API 사용할 것
+        CreateAccountPacketReq packet = new CreateAccountPacketReq() {
+            AccountName = account,
+            Password = password
+        };
+
+        Managers.Web.SendPostRequest<CreateAccountPacketRes>("account/create", packet, (res) => {
+            Debug.Log(res.CreateOk);
+
+            GetText((int)Texts.AccountNameText).text = "";
+            GetText((int)Texts.PasswordText).text = "";
+        });
     }
 
     public void OnClickLoginButton(PointerEventData evt)
     {
-        Debug.Log("OnClickLoginButton");
+        string account = GetText((int)Texts.AccountNameText).text;
+        string password = GetText((int)Texts.PasswordText).text;
+
+        LoginAccountPacketReq packet = new LoginAccountPacketReq() {
+            AccountName = account,
+            Password = password
+        };
+
+        Managers.Web.SendPostRequest<LoginAccountPacketRes>("account/login", packet, (res) => {
+            Managers.Scene.LoadScene(Define.Scene.Game);
+        });
     }
 }

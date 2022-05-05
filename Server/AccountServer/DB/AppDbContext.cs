dd0163a301207773c9c0516b1c8a848a4cf9b180
daniel <hellena171791@gmail.com>
2022-05-05 11:55:53 +0900

    <서버 - 계정서버 #1> 필요 파일 추가 (AppDbContext / DataModel / WebPacket

---
+++ Server/AccountServer/DB/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountServer.DB
+{
+    public class AppDbContext
+    {
+    }
+}

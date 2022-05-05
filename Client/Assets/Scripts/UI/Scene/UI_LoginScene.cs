5645ea863752ec417627fb8473dbd406bf83cd63
daniel <hellena171791@gmail.com>
2022-05-05 14:27:42 +0900

    <클라 - 로그인 UI> 로그인 UI 로직 구현

--- Client/Assets/Scripts/UI/Scene/UI_LoginScene.cs
+++ Client/Assets/Scripts/UI/Scene/UI_LoginScene.cs
@@ -1,11 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI_LoginScene : UI_Scene
 {
+    enum Texts
+    {
+        AccountNameText,
+        PasswordText
+    }
+
+    enum Images
+    {
+        CreateBtn,
+        LoginBtn
+    }
+
     public override void Init()
     {
         base.Init();
+
+        Bind<Text>(typeof(Texts));
+        Bind<Image>(typeof(Images));
+
+        GetImage((int)Images.CreateBtn).gameObject.BindEvent(OnClickCreateButton);
+        GetImage((int)Images.LoginBtn).gameObject.BindEvent(OnClickLoginButton);
+    }
+
+    public void OnClickCreateButton(PointerEventData evt)
+    {
+        Debug.Log("OnClickCreateButton");
+    }
+
+    public void OnClickLoginButton(PointerEventData evt)
+    {
+        Debug.Log("OnClickLoginButton");
     }
 }

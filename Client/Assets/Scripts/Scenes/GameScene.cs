91ee9537014a11ccdc606bf546813b34bc0cc2c6
daniel <hellena171791@gmail.com>
2022-05-05 13:41:25 +0900

    <클라 - 계정서버 #2> 계정 생성 테스트

--- Client/Assets/Scripts/Scenes/GameScene.cs
+++ Client/Assets/Scripts/Scenes/GameScene.cs
@@ -12,6 +12,10 @@ public class GameScene : BaseScene
 
         SceneType = Define.Scene.Game;
 
+        // TODO : 잠시 
+        Managers.Web.BaseUrl = "https://localhostL5001/api";
+        WebPacket.SendCreateAccount("Daniel", "1234");
+
         Managers.Map.LoadMap(1);
 
         Screen.SetResolution(640, 480, false);

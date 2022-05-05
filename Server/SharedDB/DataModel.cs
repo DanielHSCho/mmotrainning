963a42f35cc8714ede020a2db21f3d025091dc4a
daniel <hellena171791@gmail.com>
2022-05-05 15:33:26 +0900

    <서버 - SharedDb> 데이터 모델링 (유저 인증 토큰 / 서버 정보)

--- Server/SharedDB/DataModel.cs
+++ Server/SharedDB/DataModel.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace SharedDB
 {
-    class DataModel
+    [Table("Token")]
+    public class TokenDb
     {
+        // PK
+        public int TokenDbId { get; set; }
+        public int AccountDbId { get; set; }
+        public int Token { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    // Note : Redis로 개선할수도 있음
+    [Table("ServerInfo")]
+    public class ServerDb
+    {
+        // PK
+        public int ServerDbId { get; set; }
+        public string Name { get; set; }
+        public string IpAdress { get; set; }
+        public int Port { get; set; }
+        public int BusyScore { get; set; }
     }
 }

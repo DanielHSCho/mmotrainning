5833d3943fdacae4ec860a7c167611342ad60062
daniel <hellena171791@gmail.com>
2022-05-05 12:27:25 +0900

    <서버 - 계정서버 #1> 로그인에 필요한 패킷 설계 (+)
    - 패킷이 많지는 않으므로, 프로토버프는 사용하지 않음 (직접 패킷 구현 방식)
    - 웹서버이므로 Req가 와야함

--- Server/AccountServer/DB/WebPacket.cs
+++ Server/AccountServer/DB/WebPacket.cs
@@ -3,9 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AccountServer.DB
+public class CreateAccountPacketReq
 {
-    public class WebPacket
-    {
-    }
+    public string AccountName { get; set; }
+    public string Password { get; set; }
 }
+
+public class CreateAccountPacketRes
+{
+    // TODO : 구체적인 실패 사유가 있다면 int로
+    public bool CreateOk { get; set; }
+}
+
+public class LoginAccountPacketReq
+{
+    public string AccountName { get; set; }
+    public string Password { get; set; }
+}
+
+public class ServerInfo
+{
+    public string Name { get; set; }
+    public string Ip { get; set; }
+    public int CrowdedLevel { get; set; }
+}
+
+public class LoginAccountPacketRes
+{
+    public bool LoginOk { get; set; }
+    public List<ServerInfo> ServerList { get; set; } = new List<ServerInfo>();
+}
+
+// TODO : 대기열
+// 대기열은 웹서버일지 게임서버일지 고민한 후 구현

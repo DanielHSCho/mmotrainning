cccdf0872866347195d5d6a31a710e484320fe06
daniel <hellena171791@gmail.com>
2022-05-05 13:04:22 +0900

    <클라 - 계정서버 #2> 클라 패킷설계 (+)
    - 참고로 클라의 경우는 Json유틸리티 사용하려면 get set 있으면 인식 오류 생기므로 일반 필드로 관리

--- Client/Assets/Scripts/Web/WebPacket.cs
+++ Client/Assets/Scripts/Web/WebPacket.cs
@@ -1,8 +1,34 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
+using System.Threading.Tasks;
 
-public class WebPacket
+public class CreateAccountPacketReq
 {
-   
+    public string AccountName;
+    public string Password;
+}
+
+public class CreateAccountPacketRes
+{
+    public bool CreateOk;
+}
+
+public class LoginAccountPacketReq
+{
+    public string AccountName;
+    public string Password;
+}
+
+public class ServerInfo
+{
+    public string Name;
+    public string Ip;
+    public int CrowdedLevel;
+}
+
+public class LoginAccountPacketRes
+{
+    public bool LoginOk;
+    public List<ServerInfo> ServerList = new List<ServerInfo>();
 }

d64c0e8eee747e1321083dab58f9c0e9b5c91338
daniel <hellena171791@gmail.com>
2022-05-05 12:07:35 +0900

    <서버 - 계정서버 #1> AppDbContext 기초 작업 / StartUp에 등록

--- Server/AccountServer/DB/AppDbContext.cs
+++ Server/AccountServer/DB/AppDbContext.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AccountServer.DB
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<AccountDb> Accounts { get; set; }
+
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AccountDb>()
+                .HasIndex(a => a.AccountName)
+                .IsUnique();
+        }
     }
 }

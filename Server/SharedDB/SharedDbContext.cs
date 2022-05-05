3d6e8c171f25a0eb514d0a93cd8a7b41df1c51b0
daniel <hellena171791@gmail.com>
2022-05-05 15:33:57 +0900

    <서버 - SharedDb> SharedDbContext 구현 (ASP.Net 방식과 게임서버에서 사용하는 방식 둘다 사용)

--- Server/SharedDB/SharedDbContext.cs
+++ Server/SharedDB/SharedDbContext.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SharedDB
 {
-    class SharedDbContext
-    {
+    public class SharedDbContext : DbContext
+    { 
+        public DbSet<TokenDb> Tokens { get; set; }
+        public DbSet<ServerDb> Servers { get; set; }
+
+        public static string ConnectionString { get; set; } = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SharedDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // 게임 서버에서 사용할 방식
+        public SharedDbContext()
+        {
+
+        }
+
+        // ASP.NET 방식
+        public SharedDbContext(DbContextOptions<SharedDbContext> options) : base(options)
+        {
+
+        }
+
+        // 게임서버 방식 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options
+                //.UseLoggerFactory(_logger)
+                .UseSqlServer(ConnectionString);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TokenDb>()
+                .HasIndex(t => t.AccountDbId)
+                .IsUnique();
+
+            builder.Entity<ServerDb>()
+                .HasIndex(s => s.Name)
+                .IsUnique();
+        }
     }
 }
